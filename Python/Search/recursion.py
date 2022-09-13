1f681c06081d78e8848291d09e90bcd580e66265
gurzsky <gurski.ce@gmail.com>
2022-09-13 07:37:54 -0300

    recursion - plus book solution

--- Python/Search/recursion.py
+++ Python/Search/recursion.py
@@ -7,6 +7,14 @@ def sum(list):
 
 print(sum([1, 3, 5, 7, 9]))
 
+#book solution
+def soma(lista):
+    if lista == []:
+        return 0
+    return lista[0] + soma(lista[1:])
+
+print(soma([1, 3, 5, 7, 9]))
+
 def countItens(list, count):
     if (len(list) == 0):
         return count
@@ -15,6 +23,14 @@ def countItens(list, count):
         return countItens(list, (count + 1))
 
 print(countItens([1, 3, 5, 7, 9], 0))
+
+#book solution
+def conta(lista):
+    if lista == []:
+        return 0
+    return 1 + conta(lista[1:])
+
+print(conta([1, 3, 5, 7, 9]))
 
 def higherValue(list, value):
     if (len(list) == 0):
@@ -26,6 +42,15 @@ def higherValue(list, value):
         return higherValue(list, value)
 
 print(higherValue([1, 3, 5, 7, 9, 2, 4], 0))
+
+#book solution
+def maximo(lista):
+    if len(lista) == 2:
+        return lista[0] if lista[0] > lista[1] else lista[1]
+    sub_max = maximo(lista[1:])
+    return lista[0] if lista[0] > sub_max else sub_max
+
+print(maximo([1, 3, 5, 7, 9, 2, 4]))
 
 
 
